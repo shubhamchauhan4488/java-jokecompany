4d2a452fe10525fb5c83743a71ce3418d200b62d
shubhamchauhan <shubhamchauhan4488@gmail.com>
2024-06-10 15:13:30 -0400

    refactor category and joke interaction to encapsulate

--- src/main/java/com/jokecompany/CategoryInteraction.java
+++ src/main/java/com/jokecompany/CategoryInteraction.java
@@ -3,16 +3,24 @@ package com.jokecompany;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryInteraction extends Interaction {
     private final JokeService jokeService;
+    private List<String> categories;
 
     public CategoryInteraction(ConsolePrinter printer, BufferedReader reader, JokeService jokeService) {
         super(printer, reader);
         this.jokeService = jokeService;
+        this.categories = new ArrayList<>();
     }
 
-    public void displayCategories() throws InterruptedException, IOException, URISyntaxException {
-        printResults(jokeService.getCategories());
+    public void fetchCategories() throws InterruptedException, IOException, URISyntaxException {
+        this.categories = jokeService.getCategories();
+    }
+
+    public List<String> getCategories(){
+        return this.categories;
     }
 }
--- src/main/java/com/jokecompany/JokeInteraction.java
+++ src/main/java/com/jokecompany/JokeInteraction.java
@@ -3,16 +3,20 @@ package com.jokecompany;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JokeInteraction extends Interaction {
     private final JokeService jokeService;
+    private List<String> jokes;
 
     public JokeInteraction(ConsolePrinter printer, BufferedReader reader, JokeService jokeService) {
         super(printer, reader);
         this.jokeService = jokeService;
+        this.jokes = new ArrayList<>();
     }
 
-    public void getJokes() throws InterruptedException, IOException, URISyntaxException {
+    public void fetchJokes() throws InterruptedException, IOException, URISyntaxException {
         String categoryChoice = prompt("Want to specify a category? y/n");
         String category = "";
 
@@ -20,6 +24,10 @@ public class JokeInteraction extends Interaction {
             category = prompt("Enter a category:");
         }
         int numberOfJokes = Integer.parseInt(prompt("How many jokes do you want? (1-9)"));
-        printResults(jokeService.getRandomJokes(category, numberOfJokes));
+        this.jokes = jokeService.getRandomJokes(category, numberOfJokes);
+    }
+
+    public List<String> getJokes(){
+        return this.jokes;
     }
 }
